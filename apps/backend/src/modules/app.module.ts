ecec58c78dffc47bc75da481e234a49ade432dd0
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-11-20 17:30:19 +0100

    ft: add initial data

--- apps/backend/src/modules/app.module.ts
+++ apps/backend/src/modules/app.module.ts
@@ -4,8 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PersistanceModule } from './persistance/persistance.module';
 import { UserModule } from './user/user.module';
+import { NoteModule } from './note/note.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), PersistanceModule, UserModule, AuthModule],
+	imports: [ConfigModule.forRoot(), PersistanceModule, UserModule, AuthModule, NoteModule],
 })
 export class AppModule {}
